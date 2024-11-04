d866cbdd21fbfe1cb2c016a66a8e2cd585dcd5ac
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-11-04 11:17:06 +0000

    Add predict_price_input_widget function.

--- src/machine_learning/data_management.py
+++ src/machine_learning/data_management.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import joblib
 
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def load_house_price_data():
     df = pd.read_csv("outputs/datasets/clean_data/House_prices_records_clean.csv")
     return df
+
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
--- src/machine_learning/predict_house_price.py
+++ src/machine_learning/predict_house_price.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from src.data_management import load_pkl_file
+#from src.data_management import load_pkl_file
 from tensorflow.keras.models import load_model
 
 
-def load_house_price_predictor_model(user_input):
+def load_house_price_predictor_model(user_input, load_pkl_file):
     """
     Load and perform house price prediction
     """
-    pipeline = load_pkl_file(f"outputs/pipeline/pipeline.pkl")
+#    pipeline = load_pkl_file(f"outputs/pipeline/pipeline.pkl")
     model = load_model(f"outputs/model/house_price_predictor_model.h5")
 
     user_input_transformed = pipeline.transform(user_input)
