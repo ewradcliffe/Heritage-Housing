51e6a1e0092f5e2fc8c171e2a921e7b7a401cc9d
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-11-04 10:18:12 +0000

    Add load_house_price_predictor_model fn in src.

--- app_pages/predict_sale_price.py
+++ app_pages/predict_sale_price.py
@@ -1,9 +1,24 @@
 import streamlit as st
+import pandas as pd
 
 def predict_sale_price_body():
 
     st.write("### Predict Sale Price.")
 
     st.write(
-        f"Test"
+        f"This page displays the predicted sale prices of the clients houses, and inputs for live house price prediction."
     )
+
+    # Model Performance criteria
+    st.write(
+        f"Please note that out model meets the agreed performance criteria of R2 of at least 0.75.\n"
+        f"Train set:   "
+        f"Validation set:   "
+        f"Test Set set:   "
+    )
+
+    # Display attributes and predicted price of clients houses.
+
+
+    # Input widget for live predictions.
+
