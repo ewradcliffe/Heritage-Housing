f3431e91a86e4b28a9856e3dc4acc38aa50a3869
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-11-06 14:57:27 +0000

    Update predict_price_input_widget code after debugging.

--- app_pages/predict_sale_price.py
+++ app_pages/predict_sale_price.py
@@ -6,7 +6,7 @@ from src.machine_learning.data_management import load_house_price_data
 def predict_sale_price_body():
     # Load house price data
     #load_house_price_data = (pd.read_csv(f"outputs/datasets/most_important_features_data/X_train.csv").columns.to_list())
-    pipeline = load_pkl_file(f"outputs/pipeline_2/pipeline_2.pkl")
+    #pipeline = load_pkl_file(f"outputs/pipeline_2/pipeline_2.pkl")
 
     st.write("### Predict Sale Price.")
 
@@ -39,7 +39,6 @@ def predict_sale_price_body():
     if st.button("Get my house price."):
         # Place the inputs in the same order as the data set used to train the model.
         X_live = X_live.filter(pipeline_features)
-        print(X_live)
 
         prediction = load_house_price_predictor_model(X_live)
         st.write(f"We predict the house is worth **${prediction}**")
@@ -49,11 +48,9 @@ def predict_price_input_widget():
     """Function to display input widget"""
     # We need to load the dataset to get the min/max values for numerical inputs, and choices for catagorical data.
     df = load_house_price_data()
-    st.write(f'{df}')
     percentageMin, percentageMax = 0.4, 2.0
 
-
-    # we create input widgets for the 12 features not dropped.
+    # we create input widgets for the 5 features not dropped.
     col1, col2, col3, col4, col5 = st.beta_columns(5)
 
     # Empty dataframe for the live data.
@@ -65,8 +62,7 @@ def predict_price_input_widget():
         feature = 'KitchenQual'
         st_widget = st.selectbox(
             label=feature,
-            options=df[0]
-            #options=["Ex: Excellent", "Gd: Good", "TA: Typical/Average", "Fa: Fair; Po: Poor"]
+            options=df[feature].unique()
         )
     X_live[feature] = st_widget
 
@@ -110,6 +106,5 @@ def predict_price_input_widget():
         )
     X_live[feature] = st_widget
 
-    st.write(X_live)
-
+    #st.write(X_live)
     return X_live
--- src/machine_learning/data_management.py
+++ src/machine_learning/data_management.py
@@ -6,7 +6,7 @@ import joblib
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def load_house_price_data():
     df = pd.read_csv("outputs/datasets/most_important_features_data/X_train.csv")
-    return df,
+    return df
 
 def load_pkl_file(file_path):
     return joblib.load(filename=file_path)
