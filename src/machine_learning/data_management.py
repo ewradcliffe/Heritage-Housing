81853f0d226b2ec90718435134c6c8c3a9300d84
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-11-06 10:03:27 +0000

    Update links in src/machine_learning.

--- src/machine_learning/data_management.py
+++ src/machine_learning/data_management.py
@@ -5,7 +5,7 @@ import joblib
 
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def load_house_price_data():
-    df = pd.read_csv("outputs/datasets/training_data/X_train.csv")
+    df = pd.read_csv("outputs/datasets/most_important_features_data/X_train.csv")
     return df,
 
 def load_pkl_file(file_path):
--- src/machine_learning/predict_house_price.py
+++ src/machine_learning/predict_house_price.py
@@ -10,8 +10,8 @@ def load_house_price_predictor_model(user_input):
     Load and perform house price prediction
     """
 
-    pipeline = load_pkl_file(f"outputs/pipeline/pipeline.pkl")
-    model = load_model(f"outputs/model/house_price_predictor_model.h5")
+    pipeline = load_pkl_file(f"outputs/pipeline_2/pipeline_2.pkl")
+    model = load_model(f"outputs/model_2/house_price_predictor_model_2.h5")
 
     user_input_transformed = pipeline.transform(user_input)
     house_price_prediction = model.predict(user_input_transformed)
