f225f4b1e6602341eeaa5f8df0b35294f2b4aabd
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-11-06 13:04:57 +0000

    Add code to display predict_price_input_widget function.

--- app_pages/predict_sale_price.py
+++ app_pages/predict_sale_price.py
@@ -4,6 +4,9 @@ from src.machine_learning.predict_house_price import load_house_price_predictor_
 from src.machine_learning.data_management import load_house_price_data
 
 def predict_sale_price_body():
+    # Load house price data
+    #load_house_price_data = (pd.read_csv(f"outputs/datasets/most_important_features_data/X_train.csv").columns.to_list())
+    pipeline = load_pkl_file(f"outputs/pipeline_2/pipeline_2.pkl")
 
     st.write("### Predict Sale Price.")
 
@@ -30,10 +33,11 @@ def predict_sale_price_body():
 
     ## Display input widget
     X_live = predict_price_input_widget()
-    pipeline_features = pd.read_csv(f'outputs/datasets/training_data/X_train.csv').columns.to_list()
+    pipeline_features = pd.read_csv(f'outputs/datasets/most_important_features_data/X_train.csv').columns.to_list()
 
     # Predict on live data
     if st.button("Get my house price."):
+        # Place the inputs in the same order as the data set used to train the model.
         X_live = X_live.filter(pipeline_features)
         print(X_live)
 
@@ -45,94 +49,67 @@ def predict_price_input_widget():
     """Function to display input widget"""
     # We need to load the dataset to get the min/max values for numerical inputs, and choices for catagorical data.
     df = load_house_price_data()
+    st.write(f'{df}')
     percentageMin, percentageMax = 0.4, 2.0
 
 
     # we create input widgets for the 12 features not dropped.
-    col1, col2, col3, col4 = st.beta_columns(4)
-    col5, col6, col7, col8 = st.beta_columns(4)
-
+    col1, col2, col3, col4, col5 = st.beta_columns(5)
 
     # Empty dataframe for the live data.
     X_live = pd.DataFrame([], index=[0])
 
     # Draw the widget based on the variable type (numerical or categorical)
     # and set initial values
-    
     with col1:
-        feature = "1stFlrSF"
-        st_widget = st.number_input(
-			label= feature,
-            #min_value=df[feature].min()*percentageMin,
-            #max_value=df[feature].max()*percentageMax,
-			value= 1000
-			)
+        feature = 'KitchenQual'
+        st_widget = st.selectbox(
+            label=feature,
+            options=df[0]
+            #options=["Ex: Excellent", "Gd: Good", "TA: Typical/Average", "Fa: Fair; Po: Poor"]
+        )
     X_live[feature] = st_widget
 
     with col2:
-        feature = "2ndFlrSF"
+        feature = 'GarageArea'
         st_widget = st.number_input(
             label=feature,
-            #min_value=df[feature].min()*percentageMin,
-            #max_value=df[feature].max()*percentageMax,
-            value=300
+            min_value=df[feature].min()*percentageMin,
+            max_value=df[feature].max()*percentageMax,
+            value=df[feature].median()
         )
     X_live[feature] = st_widget
     
     with col3:
-        feature = "BedroomAbvGr"
+        feature = 'GrLivArea'
         st_widget = st.number_input(
             label=feature,
-            #min_value=df[feature].min()*percentageMin,
-            #max_value=df[feature].max()*percentageMax,
-            value= 3
+            min_value=df[feature].min()*percentageMin,
+            max_value=df[feature].max()*percentageMax,
+            value=df[feature].median()
         )
     X_live[feature] = st_widget
     
     with col4:
-        feature = "BsmtExposure"
-        st_widget = st.selectbox(
+        feature = 'OverallQual'
+        st_widget = st.number_input(
             label=feature,
-            options=3
+            min_value=df[feature].min()*percentageMin,
+            max_value=df[feature].max()*percentageMax,
+            value=df[feature].median()
         )
     X_live[feature] = st_widget
 
     with col5:
-        feature = "BsmtFinSF1"
+        feature = 'TotalBsmtSF'
         st_widget = st.number_input(
             label=feature,
-            #min_value=df[feature].min()*percentageMin,
-            #max_value=df[feature].max()*percentageMax,
-            value=0
+            min_value=df[feature].min()*percentageMin,
+            max_value=df[feature].max()*percentageMax,
+            value=df[feature].median()
         )
     X_live[feature] = st_widget
 
-    with col6:
-        feature = "BsmtFinType1"
-        st_widget = st.selectbox(
-            label=feature,
-            options=df[3]
-        )
-    X_live[feature] = st_widget
-
-    with col7:
-        feature = "BsmtUnfSF"
-        st_widget = st.number_input(
-            label=feature,
-            #min_value=df[feature].min()*percentageMin,
-            #max_value=df[feature].max()*percentageMax,
-            value=0
-        )
-    X_live[feature] = st_widget
-    
-    with col8:
-        feature = "GarageArea"
-        st_widget = st.number_input(
-            label=feature,
-            #min_value=df[feature].min()*percentageMin,
-            #max_value=df[feature].max()*percentageMax,
-            value=100
-        )
-    X_live[feature] = st_widget
+    st.write(X_live)
 
     return X_live
