b169a09badbadb993295eaa8055b4de6f01eda65
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-11-08 11:44:53 +0000

    Update predict_sale_price code to be pep8 compliant.

--- app_pages/predict_sale_price.py
+++ app_pages/predict_sale_price.py
@@ -1,53 +1,63 @@
 import streamlit as st
 import pandas as pd
-from src.machine_learning.predict_house_price import load_house_price_predictor_model
+from src.machine_learning.predict_house_price import \
+    load_house_price_predictor_model
 from src.machine_learning.data_management import load_house_price_data
 
+
 def predict_sale_price_body():
-    st.write("### Predict Sale Price.")
-
+    # Function to render page.
+    st.header("**Predict Sale Price.**")
     st.write(
-        f"The client is interested in predicting the house sale prices from her 4 inherited houses,"
-        f"and any other house in Ames, Iowa."
+        f"The client is interested in predicting the house sale prices from "
+        f"her 4 inherited houses, and any other house in Ames, Iowa."
     )
     st.write(
-        f"This page displays the sale prices of the clients houses predicted with our model, and inputs"
-        f"for live house price prediction using the same model."
+        f"This page displays the sale prices of the clients houses predicted "
+        f"with our model, and inputs for live house price prediction using "
+        f"the same model."
     )
-
-    st.write("---")
 
     # Display attributes and predicted price of clients houses.
-    st.write(f"The client provided us with a data set for the houses she inherited."
-            f"We used our model to predict the price using the most important features"
-            f"as detailed on the Project Findings page")
+    st.subheader(f"**Inherited house price predictions**")
+    st.write(f"The client provided us with a data set for the houses she "
+             f"inherited. We used our model to predict the price using the "
+             f"most important features as detailed on the Project Findings "
+             f"page.")
     st.write(f"The results are displayed below.")
 
-    inherited_houses_prediction_df = (pd.read_csv(f'outputs/datasets/predicted_prices_2/house_price_predictions_2.csv'))
-    inherited_houses_prediction_df.rename(columns={'0': 'Price (USD)'}, inplace=True)
+    inherited_houses_prediction_df = (
+        pd.read_csv(
+            f"outputs/datasets/predicted_prices_2/house_price_predictions_2.csv"
+                    ))
+    inherited_houses_prediction_df.rename(
+        columns={'0': 'Price (USD)'}, inplace=True)
     st.write(inherited_houses_prediction_df)
 
-    st.write("---")
-    
     # Input widget for live predictions.
+    st.subheader(f"**Predict a house price.**")
     st.write(f"To predict the price of any other house using the same model,"
-            f"please enter the details below.")
+             f"please enter the details below.")
 
     # Display input widget
     X_live = predict_price_input_widget()
-    pipeline_features = pd.read_csv(f'outputs/datasets/most_important_features_data/X_train.csv').columns.to_list()
+    pipeline_features = pd.read_csv(
+        f'outputs/datasets/most_important_features_data/X_train.csv'
+        ).columns.to_list()
 
     # Predict on live data
     if st.button("Get my house price"):
-        # Place the inputs in the same order as the data set used to train the model.
+        # Place the inputs in the same order as the data
+        # set used to train the model.
         X_live = X_live.filter(pipeline_features)
         prediction = load_house_price_predictor_model(X_live)
         st.write(f"We predict the house is worth **${prediction}**")
 
 
 def predict_price_input_widget():
-    """Function to display input widget"""
-    # Load the dataset to get choices for KitchenQual and start value for OverallQual.
+    # Function to display input widget"""
+    # Load the dataset to get choices for KitchenQual
+    # and start value for OverallQual.
     df = load_house_price_data()
 
     # Empty dataframe for the live data.
@@ -81,7 +91,8 @@ def predict_price_input_widget():
         st.write(f'Ex: Excellent; Gd: Good; TA: Typical/Average; Fa: Fair')
 
     with col4:
-        st.write(f'Please grade the overall quality of the house on a scale of 1 - 10')    
+        st.write(f'Please grade the overall quality of the house on a scale ')
+        st.write(f'of 1 - 10')
 
     feature = 'GarageArea'
     st_widget = st.slider(
@@ -94,7 +105,7 @@ def predict_price_input_widget():
     X_live[feature] = st_widget
 
     st.write(f'Size of garage in square feet')
-    
+
     feature = 'GrLivArea'
     st_widget = st.slider(
         label=feature,
