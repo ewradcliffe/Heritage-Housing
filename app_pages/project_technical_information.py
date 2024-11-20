24b92d3b6f4cf3314b68661a8a63917f3ba40a6b
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-11-20 16:43:24 +0000

    Add pipeline and model specs to technical information.

--- app_pages/project_technical_information.py
+++ app_pages/project_technical_information.py
@@ -1,42 +1,91 @@
 import streamlit as st
 
+
 def project_technical_information_body():
-    #Renders technical information page.
+    # Renders technical information page.
 
     st.header("**Technical Information**")
-    st.write(f"This page shows information which may be useful for data analysts and "
-             f"developers."
+    st.write(
+        f"This page shows information which may be useful for data analysts "
+        f"and developers."
     )
-    # Model Performance criteria. Please note that although we have trained two models
-    # we are only providing data for the deployed model. The information is hard copied
-    # from 07 - ModellingMostImportantFeatures
+    # Model Performance criteria. Please note that although we have trained two
+    # models we are only providing data for the deployed model. The information
+    # is hard copiedfrom 07 - ModellingMostImportantFeatures
     st.subheader(f"Model Performance")
     st.write(f"**Train Set**")
     st.write(
-        f"Please see below for details of the performance of train, validation and "
-        f"test data sets. Please note that our model meets the agreed performance "
-        f"criteria of R2 of at least 0.75."
+        f"Please see below for details of the performance of train, validation"
+        f" and test data sets. Please note that our model meets the agreed "
+        f"performance criteria of R2 of at least 0.75."
         )
 
     # Train set
     st.write(f"**Train Set**")
-    st.write(f"R2 Score: 0.843  \n Mean Absolute Error: 20617.138  \n"
-            f"Mean Squared Error: 989369562.043  \n Root Mean Squared Error: 31454.246"
+    st.write(
+        f"R2 Score: 0.843  \n Mean Absolute Error: 20617.138  \n"
+        f"Mean Squared Error: 989369562.043  \n Root Mean Squared Error: "
+        f"31454.246"
             )
 
     # Validation set.
     st.write(f"**Validation Set**")
-    st.write(f"R2 Score: 0.849  \n Mean Absolute Error: 21183.536  \n"
-            f"Mean Squared Error: 829900380.727  \n Root Mean Squared Error: 28807.992"
+    st.write(
+        f"R2 Score: 0.849  \n Mean Absolute Error: 21183.536  \n"
+        f"Mean Squared Error: 829900380.727  \n Root Mean Squared Error: "
+        f"28807.992"
             )
 
     # Test set.
-    st.subheader(f"**Test Set**")
-    st.write(f"R2 Score: 0.779  \n Mean Absolute Error:22401.275  \n"
-            f"Mean Squared Error: 1529169051.688  \n Root Mean Squared Error: 39104.591"
-            )
+    st.write(f"**Test Set**")
+    st.write(
+        f"R2 Score: 0.779  \n Mean Absolute Error:22401.275  \n"
+        f"Mean Squared Error: 1529169051.688  \n Root Mean Squared Error: "
+        f"39104.591"
+        )
 
-    # Pipeline steps
+    # Pipeline steps. Based on the pipeline in 07 -
+    # ModellingMostImportantFeatures
+    st.subheader(f"**Pipeline steps**")
+    st.write(f"The steps below were taken to prepare the data.")
 
+    st.write(
+        f"In the data set provided EnclosedPorch and WoodDeckSF had 90.7% and "
+        f"89.4% of their respective data missing. These features were removed "
+        f"before any other steps were taken as there was too much missing "
+        f"data to reliably impute and it was not possible to impute the "
+        f"missing data from another category."
+        )
 
-    # Other technical information
+    st.write(
+        f"The features identified as strongly or very strongly correlated with"
+        f" SalePrice (GarageArea, GrLivArea, KitchenQual, OverallQual, "
+        f"TotalBsmtSF) were extracted from the data set. Other features were "
+        f"dropped."
+        )
+    st.write(f"The data was split into train, validate and test sets.")
+    st.write(f"OrdinalCategoricalEncoder was applied to KitchenQual.")
+    st.write(
+        f"PowerTransformer was applied to GarageArea, GrLivArea,  OverallQual,"
+        f" TotalBsmtSF."
+        )
+    st.write(
+        f"The data was then Windsorised to cap outliers based on the Inter "
+        f"Quartile Range (IQR)."
+        )
+    st.write(f"The data was scaled with StandardScaler")
+
+    # The Model
+    st.subheader(f"The model")
+    st.write(f"The model itself consists of a neural network.")
+    st.write(f"The input layer is equal to the number of features (i.e. 5).")
+    st.write(
+        f"This is followed by four hidden layers of 512 nodes, each "
+        f"alternating with a droput layer set to 0.25."
+        )
+    st.write(f"Activation functions are all rectified linear unit (ReLU).")
+    st.write(
+        f"As we created a linear regression model, the output layer consisted "
+        f"of one node, The loss function was Mean Squared Error (MSE), the "
+        f"optimizer was Adam."
+        )
