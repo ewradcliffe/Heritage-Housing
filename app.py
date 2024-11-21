abb890d3552599506ce29b5b50b12ef31e4470aa
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-11-21 11:30:21 +0000

    Update app.py to be PEP8 compliant.

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ from app_pages.project_summary import page_summary_body
 from app_pages.project_findings import project_findings_body
 from app_pages.predict_sale_price import predict_sale_price_body
 from app_pages.project_hypothosis import project_hypothosis_body
-from app_pages.project_technical_information import project_technical_information_body
+from app_pages.project_technical_information import  \
+     project_technical_information_body
 
 
-app = MultiPage(app_name= "House Price Predictions") # Create an instance of the app 
+# Create an instance of the app
+app = MultiPage(app_name="House Price Predictions")
 
 # Add your app pages here using .add_page()
 app.add_page("Project Summary", page_summary_body)
@@ -18,5 +20,5 @@ app.add_page("Predict Sale Price", predict_sale_price_body)
 app.add_page("Project Hypothosis", project_hypothosis_body)
 app.add_page("Technical Information", project_technical_information_body)
 
-
-app.run() # Run the  app
+# Run the  app
+app.run()
